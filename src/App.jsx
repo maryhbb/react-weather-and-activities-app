596d942ade1bacc7b56f0630905e66eeaaf0b98d
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-30 22:53:05 +0200

    Implement filtering the List

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./index.css";
 import "./App.css";
 import Form from "./components/Form/index.jsx";
 import useLocalStorageState from "use-local-storage-state";
+import List from "./components/List";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
@@ -10,15 +11,19 @@ function App() {
 
   function handleAddActivity(newActivity) {
     setActivities(previousActivities => [...previousActivities, newActivity]);
-
-    console.log("newActivity:", newActivity);
   }
+
+  const isGoodWeather = false;
+  const filteredActivities = activities.filter(
+    activity => activity.isForGoodWeather === isGoodWeather,
+  );
 
   return (
     <>
       <div className="App">
         <h1>Wheather & Activities App</h1>
         <Form onAddActivity={handleAddActivity} />
+        <List activities={filteredActivities} />
       </div>
     </>
   );
