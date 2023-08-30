a943d6a1dc1c0d788d9472cf3a9a0b209dc8cedd
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-31 01:27:45 +0200

    Fetch weather data

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,66 @@ import "./App.css";
 import Form from "./components/Form/index.jsx";
 import useLocalStorageState from "use-local-storage-state";
 import List from "./components/List";
+import { useEffect, useState } from "react";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
 
+  const [weather, setWeather] = useState(null);
+
   function handleAddActivity(newActivity) {
     setActivities(previousActivities => [...previousActivities, newActivity]);
   }
 
-  const isGoodWeather = false;
+  function handleDeleteActivity(id) {
+    console.log("delete Item with id: ", id);
+    setActivities(previousActivities =>
+      previousActivities.filter(activity => activity.id !== id),
+    );
+  }
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather/europe",
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setWeather(data);
+        } else {
+          console.error("Failed!!");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchWeather();
+    const timer = setInterval(fetchWeather, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const isGoodWeather = weather?.isGoodWeather ?? false;
+  console.log(weather);
+
   const filteredActivities = activities.filter(
     activity => activity.isForGoodWeather === isGoodWeather,
   );
+
+  console.log(filteredActivities);
+
+  if (!weather) {
+    return (
+      <div className="app-loading">
+        <strong>Loading...</strong>
+      </div>
+    );
+  }
 
   return (
     <>
