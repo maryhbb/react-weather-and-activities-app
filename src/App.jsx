7452cfb268aa631b65130d4500d5d5c216d84693
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-30 19:08:15 +0200

    init Weather & Activities App project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+function App() {
+  return (
+    <>
+      <h1>Weather & Activities App</h1>
+      <p>Use this as a starting point to develop your own application :-)</p>
+    </>
+  );
+}
+
+export default App;
